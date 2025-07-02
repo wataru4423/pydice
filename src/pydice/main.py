0b4d6cf1b60f791c776ffa45874d5ea514c9af54
Wataru Enya <ewataru4423@gmail.com>
2025-07-02 15:57:15 +0900

    feat: Enhance main function to support returning each die value

--- src/pydice/main.py
+++ src/pydice/main.py
@@ -15,10 +15,12 @@ def main(
         int, typer.Option(min=1, max=100, help="Number of dice to roll")
     ] = 1,
     weight: Annotated[bool, typer.Option(help="Weighted dice")] = False,
+    each: Annotated[bool, typer.Option(help="Return each die value")] = False,
 ):
     """Dice roll application.\n
-    Default: to roll a 6-sided dice.\n
-    If --weight is set, last number will be weighted to 3.
+    Default: to roll a 6-sided dice and return sum value.\n
+    If --weight is set, last number will be weighted to 3.\n
+    If --each is set, each die value will be returned instead of the sum.
     """
 
     def roll(bones, pairs, weight):
@@ -33,6 +35,10 @@ def main(
         else:
             dice = random.choices(die, k=pairs)
 
-        return sum(dice)
+        return dice
 
-    print(roll(bones, pairs, weight))
+    rolls = roll(bones, pairs, weight)
+    if each:
+        print(*rolls, sep=", ")
+    else:
+        print(sum(rolls))
