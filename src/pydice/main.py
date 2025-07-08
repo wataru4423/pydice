a940fe1e163449ac345a385c7f613f60570e7b82
Wataru Enya <ewataru4423@gmail.com>
2025-07-08 20:31:50 +0900

    feat: Add dice format validation and update main function signature

--- src/pydice/main.py
+++ src/pydice/main.py
@@ -1,4 +1,5 @@
 import random
+import re
 from typing import Annotated
 
 import typer
@@ -8,6 +9,7 @@ app = typer.Typer()
 
 @app.command()
 def main(
+    dice: Annotated[str, typer.Argument(help="Dice to roll, e.g. 2d6")] = "1d6",
     bones: Annotated[
         int, typer.Option(min=1, max=1000, help="Number of sides on the dice")
     ] = 6,
@@ -22,6 +24,12 @@ def main(
     If --weight is set, last number will be weighted to 3.\n
     If --each is set, each die value will be returned instead of the sum value.
     """
+
+    if not re.fullmatch(r"(\d{1,2}|100)[d](\d{1,3}|1000)", dice):
+        print("Invalid dice format. Please use the format like 2d6.")
+        raise typer.Exit(code=1)
+
+    pairs, bones = map(int, dice.split("d"))
 
     rolls = roll(bones, pairs, weight)
     if each:
